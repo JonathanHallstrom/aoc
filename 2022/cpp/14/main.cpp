2aa3d20bdf83a8b4f47e548e6d4b144c8a067ef3
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-07-15 21:35:21 +0200

    [2022] day 14 (C++)

---
+++ 2022/cpp/14/main.cpp
@@ -0,0 +1,116 @@
+#include <algorithm>
+#include <array>
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <tuple>
+#include <vector>
+
+using position = std::complex<int>;
+
+struct comp {
+    bool operator()(position const &a, position const &b) const {
+        return std::tuple(a.real(), a.imag()) < std::tuple(b.real(), b.imag());
+    }
+};
+
+using map = std::map<position, bool, comp>;
+
+int part_one(map board) {
+    position lowest_pos = board.begin()->first;
+    for (auto [p, _]: board)
+        if (p.real() > lowest_pos.real())
+            lowest_pos = p;
+
+    const position down_left{1, -1}, down{1, 0}, down_right{1, 1};
+
+    for (int sand_count = 0;; ++sand_count) {
+        position sand_pos{0, 500};
+        while (sand_pos.real() <= lowest_pos.real()) {
+            if (board[{sand_pos + down}]) {
+                if (board[{sand_pos + down_left}]) {
+                    if (board[{sand_pos + down_right}]) {
+                        goto brk;
+                    } else {
+                        sand_pos += down_right;
+                    }
+                } else {
+                    sand_pos += down_left;
+                }
+            } else {
+                sand_pos += down;
+            }
+        }
+    brk:;
+        if (sand_pos.real() > lowest_pos.real()) {
+            return sand_count;
+        } else {
+            board[sand_pos] = true;
+        }
+    }
+    return -1;
+}
+
+int part_two(map board) {
+    const position down_left{1, -1}, down{1, 0}, down_right{1, 1};
+    position lowest_pos = board.begin()->first;
+    for (auto [p, _]: board)
+        if (p.real() > lowest_pos.real())
+            lowest_pos = p;
+    lowest_pos += down;
+
+    for (int sand_count = 0;; ++sand_count) {
+        position sand_pos{0, 500};
+        if (board[sand_pos])
+            return sand_count;
+        while (sand_pos.real() < lowest_pos.real()) {
+            if (board[{sand_pos + down}]) {
+                if (board[{sand_pos + down_left}]) {
+                    if (board[{sand_pos + down_right}]) {
+                        goto brk;
+                    } else {
+                        sand_pos += down_right;
+                    }
+                } else {
+                    sand_pos += down_left;
+                }
+            } else {
+                sand_pos += down;
+            }
+        }
+    brk:;
+        board[sand_pos] = true;
+    }
+    return -1;
+}
+
+int main() {
+    map board;
+
+    std::string s;
+    while (std::getline(std::cin, s)) {
+        std::istringstream iss(s);
+        char comma;
+        int a, b;
+        iss >> a >> comma >> b;
+        position start{b, a};
+        board[start] = true;
+        std::string dummy;
+        while (iss >> dummy >> a >> comma >> b) {
+            position p{b, a};
+            position diff = p - start;
+            diff /= std::hypot(diff.real(), diff.imag());
+
+            while (start != p) {
+                start += diff;
+                board[start] = true;
+            }
+        }
+    }
+
+    std::cout << "Part one: " << part_one(board) << '\n';
+    std::cout << "Part two: " << part_two(board) << '\n';
+
+}
